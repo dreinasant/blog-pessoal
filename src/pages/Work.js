0c2ba8e874d408b2ebb0f77b958b07935fa6c33c
dreinasant <ao.andreina@gmail.com>
2022-07-20 19:33:45 -0300

    atualizaçãoo

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -18,7 +18,7 @@ export default class Works extends Component {
                 <h2 className="botao-categorias" >Works</h2>
                 
                   <div className="box-butom" >
-                  <a title="Pou" href="https://projeto-pou-contador.netlify.app/">Pou</a>
+                  <a title="Pou" href="https://projeto-pou-contador.netlify.app/" target="_blank">Pou</a>
                   <a title="Projeto objetivos sustentável" href="https://projeto-objetivos-sustentavel.netlify.app" target="_blank">Projeto objetivos sustentável</a>
                   <a title="Filmes api" href="https://projeto-api-filmes-series.netlify.app/" target="_blank">Projeto filmes api</a>
                   <a title="Projeto de inglês dog e cat" href="https://projeto-de-ingles-dog-cat.netlify.app/" target="_blank">Projeto de inglês dog e cat</a>
