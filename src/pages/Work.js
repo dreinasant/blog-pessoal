e10728b8ffb733aa3a57d8c1bb3c7e85dbd62b90
dreinasant <ao.andreina@gmail.com>
2022-07-20 23:08:39 -0300

    novas correções

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -18,11 +18,11 @@ export default class Works extends Component {
                 <h2 className="botao-categorias" >Works</h2>
                 
                   <div className="box-butom" >
-                  <a title="Pou" href="https://projeto-pou-contador.netlify.app/" target="_blank">Pou</a>
-                  <a title="Projeto objetivos sustentável" href="https://aquamarine-gnome-5a8165.netlify.app/" target="_blank">Projeto objetivos sustentável</a>
-                  <a title="Filmes api" href="https://projeto-api-filmes-series.netlify.app/" target="_blank">Projeto filmes api</a>
-                  <a title="Projeto de inglês dog e cat" href="https://projeto-de-ingles-dog-cat.netlify.app/" target="_blank">Projeto de inglês dog e cat</a>
-                  <a title="projeto de filmes e séries" href="https://projeto-de-filmes.netlify.app/" target="_blank">Projeto de filmes e Series</a>
+                  <a title="Pou" target="blank" href="https://projeto-pou-contador.netlify.app/">Pou</a>
+                  <a title="Projeto objetivos sustentável" target="blank" href="https://aquamarine-gnome-5a8165.netlify.app/">Projeto objetivos sustentável</a>
+                  <a title="Filmes api" target="blank" href="https://projeto-api-filmes-series.netlify.app/">Projeto filmes api</a>
+                  <a title="Projeto de inglês dog e cat" target="blank" href="https://projeto-de-ingles-dog-cat.netlify.app/">Projeto de inglês dog e cat</a>
+                  <a title="projeto de filmes e séries" target="blank" href="https://projeto-de-filmes.netlify.app/">Projeto de filmes e Series</a>
                   </div>
                 
               </div>
